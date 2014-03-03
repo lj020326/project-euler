350d9d92f608e0e0b35b9071c8d78a44c16f3f2e
nidhinp <nidhinp.412@gmail.com>
2014-03-04 01:27:49 +0530

    deleted some

--- problem6.py
+++
@@ -1,3 +0,0 @@
-#The difference between the sum of the squares of the first one hundred natural numbers and the squares ofthe sum.
-
-print ((sum(range(1, 101))) ** 2) - sum([x*x for x in range(1, 101)])
--- problem9.py
+++
@@ -1,4 +0,0 @@
-#special pythagorean triplet
-
-for x in [a * b * c for a in range(450) for b in range(450) for c in range(450) if ((a < b < c) and (a**2 + b**2 == c **2) and (a + b + c == 1000)) ]:
-	print x
